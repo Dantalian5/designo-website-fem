68372e84a6ada6c2925242bccdcf99effe9a075b
Marcos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-11-11 18:12:35 +0100

    Initialice project
    - Add assets
    - Add folder srtuct
    - Add Astro fw
    - Add navbar component
    - Add tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [sentry(), spotlightjs()],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                },
+            },
+        },
+    },
+});
